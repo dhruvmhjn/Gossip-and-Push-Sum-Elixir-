9f31ff8a4f618b4d9b98a050c44cad72bc04752d
Dhruv Mahajan <dhruvmhjn@gmail.com>
2017-09-30 14:37:21 -0400

    Serv cha

--- lib/server.ex
+++ lib/server.ex
@@ -3,4 +3,10 @@ defmodule Server do
     def handle_call(:next_number, _from, current_number) do
     { :reply, current_number, current_number+1 }
     end
+
+
+    def handle_call({:set_number, new_number}, _from, _current_number) do
+    { :reply, new_number, new_number }
+    end
+
 end

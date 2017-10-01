08cb463102ae5ccd62d750ba642be438351b8a0c
Dhruv Mahajan <dhruvmhjn@gmail.com>
2017-10-01 14:39:39 -0400

    Renaming

--- lib/networksupervisor.ex
+++ lib/networksupervisor.ex
@@ -8,7 +8,7 @@ defmodule NetworkSupervisor do
         numnodes = String.to_integer(n) 
         n_miners = Enum.to_list 1..numnodes
         IO.inspect n_miners
-        children = Enum.map(n_miners, fn(x)->worker(Genplay, [top,numnodes,x], [id: "node#{x}"]) end)
+        children = Enum.map(n_miners, fn(x)->worker(GossipNode, [top,numnodes,x], [id: "node#{x}"]) end)
         IO.inspect children
         supervise children, strategy: :one_for_one
     end

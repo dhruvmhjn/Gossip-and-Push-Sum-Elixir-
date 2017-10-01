653a19b72f132b971afbb1727f1c0e021f2db832
Dhruv Mahajan <dhruvmhjn@gmail.com>
2017-10-01 01:47:13 -0400

    Bugfix

--- lib/genplaysup.ex
+++ lib/genplaysup.ex
@@ -8,8 +8,8 @@ defmodule Genplaysup do
         numnodes = String.to_integer(n) 
         n_miners = Enum.to_list 1..numnodes
         IO.inspect n_miners
-        children = Enum.map(n_miners, fn(x)->worker(Genplay, [top,n,x], [id: "node#{x}"]) end)
-        IO.puts "this is in supervisior"
+        children = Enum.map(n_miners, fn(x)->worker(Genplay, [top,numnodes,x], [id: "node#{x}"]) end)
+        IO.inspect children
         supervise children, strategy: :one_for_one
     end
 end

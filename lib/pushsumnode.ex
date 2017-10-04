9854af5118b579cb45d85df9e951b539eb39f168
blackthorn39 <blackthorn_39@live.in>
2017-10-04 19:56:48 -0400

    handle rumour

--- lib/pushsumnode.ex
+++ lib/pushsumnode.ex
@@ -1,5 +1,5 @@
 defmodule PushsumNode do
-    use Genserver
+    use GenServer
 
     def start_link(top,n,x) do
         sqn=round(:math.sqrt(n))
@@ -36,7 +36,23 @@ defmodule PushsumNode do
           randj = :rand.uniform(sqn)
           list = list ++ [String.to_atom("node#{randi}@#{randj}")]
         end
-       # state: num modes, list of neighbours, last s/w,counter,s,w
+       # state: num modes, list of neighbours, last s/w, termination counter, s, w
         {:ok,{n,list,nil,0,x,1}}
     end
+
+    def handle_cast({:rumour,s1,w1},{n,list,ratio,t_counter,s,w}) do
+        if (t_counter < 3) do
+            s = s + s1
+            w = w + w1
+            newratio = Float.round(s/w,9)
+            {t_counter,ratio} = cond do
+                abs(ratio-newratio) < :math.pow(10,-10) -> {t_counter+1,newratio}
+                true -> {0,newratio}
+            end
+            # start spreading the rumour -> cast to self 
+        end
+        {:noreply,{n,list,ratio,t_counter,s,w}}
+    end
+
+    # def handle_cast({:spread_rumour,t_counter}{})
 end
